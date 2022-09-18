5c4f832dc81746f7144c173a19e963854c06f331
Jefferson Duarte <jeffersonsduarte@live.com>
2022-09-18 23:45:12 +0100

    Configurado envio de email do form contato

--- contato/views.py
+++ contato/views.py
@@ -1,6 +1,11 @@
+from django.template.loader import render_to_string
+from django.http import HttpResponse
+from core import settings
 from django.shortcuts import render
 from .contato_form import FormContato
 from django.contrib import messages
+from django.core.mail import send_mail, BadHeaderError, EmailMultiAlternatives
+from django.utils.html import strip_tags
 
 
 def contato(request):
@@ -11,10 +16,47 @@ def processa_contato(request):
     if request.method == 'POST':
         contato = FormContato(request.POST)
         if contato.is_valid():
-            assunto = contato.cleaned_data['assunto']
-            messages.success(request, 'Mensagem enviado com sucesso!')
+            try:
+                # enviar_email(contato)
+                enviar_email_com_template(contato)
+                messages.success(request, 'Mensagem enviado com sucesso!')
+                return render(request, 'contato/contato.html', {'form': FormContato()})  # noqa
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+
             return render(request, 'contato/contato.html', {'form': FormContato()})  # noqa
         else:
             return render(request, 'contato/contato.html', {'form': contato})
 
     return render(request, 'contato/contato.html', {'form': FormContato()})
+
+
+def enviar_email(contato):
+    send_mail(
+        contato.cleaned_data['assunto'],
+        contato.cleaned_data['mensagem'],
+        settings.EMAIL_HOST_USER,
+        [contato.cleaned_data['email']],
+        fail_silently=False
+    )
+
+
+def enviar_email_com_template(contato):
+    html_content = render_to_string(
+        'email_templates/confirmacao_mensagem.html',
+        {
+            'nome': contato.cleaned_data['nome'],
+            'assunto': contato.cleaned_data['assunto'],
+        }
+    )
+    text_content = strip_tags(html_content)
+    msg = EmailMultiAlternatives(
+        contato.cleaned_data['assunto'],
+        text_content,
+        settings.EMAIL_HOST_USER,
+        [contato.cleaned_data['email']]
+    )
+    msg.attach_alternative(html_content, "text/html")
+    msg.send()
+
+    return True
--- core/settings.py
+++ core/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,7 +46,7 @@ INSTALLED_APPS = [
     'blog',
     'autenticacao',
     'contato',
-    'cursos', 
+    'cursos',
     'exemplos',
     'ckeditor',
     'ckeditor_uploader',
@@ -143,3 +147,13 @@ CKEDITOR_UPLOAD_PATH = 'uploads/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# google config
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'default from email'
